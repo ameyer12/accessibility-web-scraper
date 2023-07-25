f3ec2d5e24a596877fedd82d2f9b420da87a209e
Austin <austinmeyer1106@gmail.com>
2023-07-25 09:28:45 -0500

    working on implementing of parallelization

--- src/api/api.py
+++ src/api/api.py
@@ -86,24 +86,24 @@ def automateSearch(link):
             "alert_descriptions": [],
         },
     }
-    # Setting webdriver options
-    options = webdriver.ChromeOptions()
-    options.binary_location = os.environ.get("/Applications/your_path/Google Chrome.app/Contents/MacOS/Google Chrome")
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-infobars")      
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--enable-network-cache=true")
-    options.add_argument("--page-load-strategy=none") 
+    # # Setting webdriver options
+    # options = webdriver.ChromeOptions()
+    # options.binary_location = os.environ.get("/Applications/your_path/Google Chrome.app/Contents/MacOS/Google Chrome")
+    # options.add_argument("--headless")
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-infobars")      
+    # options.add_argument("--disable-popup-blocking")
+    # options.add_argument("--enable-network-cache=true")
+    # options.add_argument("--page-load-strategy=none") 
 
-    # # Setting executable path
-    # service = Service(ChromeDriverManager().install())
-    service = Service()
+    # # # Setting executable path
+    # # service = Service(ChromeDriverManager().install())
+    # service = Service()
     # Variable for webdriver browser
-    browser = webdriver.Chrome(service=service, options=options)
+    browser = get_thread_local_driver()
 
     # Making initial get request to WAVE website
     browser.get("https://wave.webaim.org")
