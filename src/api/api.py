6c03863e28e5b1958b6e2a9d0806cd0ae1f58b33
Austin <austinmeyer1106@gmail.com>
2023-07-24 20:49:45 -0500

    troubleshooting integration

--- src/api/api.py
+++ src/api/api.py
@@ -63,7 +63,7 @@ def automateSearch(link):
     }
     # Setting webdriver options
     options = webdriver.ChromeOptions()
-    options.binary_location = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+    options.binary_location = os.environ.get('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome')
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
